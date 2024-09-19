da963c4aa7b0dacc7ba6bbd0024a323d2faba450
Debombii <158999291+Debombii@users.noreply.github.com>
2024-09-19 08:53:19 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -166,7 +166,7 @@ const ChangelogGenerator = () => {
     formData.append('file', blob, 'changelog.html');
     formData.append('company', selectedCompany);  // Agrega la compañía seleccionada al FormData
 
-    fetch('https://flask-8cb7.onrender.com', {
+    fetch('https://flask-delta-wine.vercel.app/', {
       method: 'POST',
       body: formData
     })
