675b2648a54ec5c3b6a7253ea0c32eb0dd68f339
Torrejon Boris <66794183+TASconsultoriac@users.noreply.github.com>
2024-09-20 12:43:31 +0200

    up

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import App from "./App";
-import TestPage from "./TestPage"; // Importa tu nuevo componente para el subdominio
 
-const App = () => {
+const MainApp = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/Test" component={TestPage} />
-        <Route path="/" component={ChangelogGenerator} /> {/* Ruta principal */}
+        <Route path="/MRG">
+          <Redirect to="/template_MRG.html" />
+        </Route>
+        <Route path="/GERP">
+          <Redirect to="/template_GERP.html" />
+        </Route>
+        <Route path="/RUBICON">
+          <Redirect to="/template_Rubi.html" />
+        </Route>
+        <Route path="/" component={App} /> {/* Ruta principal */}
       </Switch>
     </Router>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<MainApp />);
