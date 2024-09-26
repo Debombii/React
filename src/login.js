41d45232e2f80a69076a4ced6afee604b4f81631
Torrejon Boris <66794183+TASconsultoriac@users.noreply.github.com>
2024-09-26 08:54:00 +0200

    asdasds

--- src/login.js
+++ src/login.js
@@ -18,23 +18,26 @@ const Login = () => {
   };
 
   return (
-      <div className="login-box">
-        <h2>Login</h2>
-        <form onSubmit={handleLogin}>
-          <input
-            type="text"
-            placeholder="Usuario"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="Conbtraseña"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button type="submit">Iniciar sesión</button>
-        </form>
+      <div className="Container">
+          <div class="circles"></div>
+        <div className="login-box">
+            <h2>Login</h2>
+            <form onSubmit={handleLogin}>
+            <input
+                type="text"
+                placeholder="Usuario"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+                type="password"
+                placeholder="Conbtraseña"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit">Iniciar sesión</button>
+            </form>
+        </div>
       </div>
   );
 };
