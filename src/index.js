7eca9153af837396de95e2dedea4287413e0e073
Torrejon Boris <66794183+TASconsultoriac@users.noreply.github.com>
2024-09-26 08:28:36 +0200

    asdasds

--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
-import React, { useEffect } from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
-import App from "./App"; // Asegúrate de que App.js exporte tu componente principal
-
-const RedirectToHTML = ({ template }) => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    // Redirigir a la página HTML deseada
-    window.location.href = `/template_${template}.html`;
-  }, [template, navigate]);
-
-  // Mientras se redirige, puedes mostrar un mensaje o nada
-  return null;
-};
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import App from './App';
+import Login from './Login';
+import ProtectedRoute from './ProtectedRoute';
 
 const MainApp = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/MRG" element={<RedirectToHTML template="MRG" />} />
-        <Route path="/GERP" element={<RedirectToHTML template="GERP" />} />
-        <Route path="/RUBICON" element={<RedirectToHTML template="Rubi" />} />
-        {/* Agrega más rutas según sea necesario */}
-        <Route path="/" element={<App />} /> {/* Ruta principal */} 
+        <Route path="/login" element={<Login />} /> {/* Ruta de Login */}
+
+        {/* Rutas protegidas */}
+        <Route 
+          path="/" 
+          element={
+            <ProtectedRoute>
+              <App /> {/* Ruta principal solo accesible si está autenticado */}
+            </ProtectedRoute>
+          } 
+        />
+
+        <Route path="/MRG" element={<Navigate to="/template_MRG.html" />} />
+        <Route path="/GERP" element={<Navigate to="/template_GERP.html" />} />
+        <Route path="/RUBICON" element={<Navigate to="/template_Rubi.html" />} />
       </Routes>
     </Router>
   );
 };
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<MainApp />);
