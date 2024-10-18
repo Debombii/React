04eb4dbb94a2b9afb848d02392b5c72c05b55e2e
Debombii <158999291+Debombii@users.noreply.github.com>
2024-10-18 10:52:21 +0200

    Update login.js

--- src/login.js
+++ src/login.js
@@ -1,41 +1,52 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './login.css';
+
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
+
+  const ADMIN_USER = process.env.NEXT_PUBLIC_ADMIN_USER;
+  const ADMIN_PASSWORD = process.env.NEXT_PUBLIC_ADMIN_PASSWORD;
+
   const handleLogin = (e) => {
     e.preventDefault();
-    if (username === 'admin' && password === '12345') {
+    if (username === ADMIN_USER && password === ADMIN_PASSWORD) {
       localStorage.setItem('isAuthenticated', 'true');
       navigate('/');
     } else {
       alert('Credenciales incorrectas');
     }
   };
+
   return (
-      <div  class="body">
-          <div class="login_circles"></div>
-        <div className="login-box">
-            <h2 class="login">Login</h2>
-            <form onSubmit={handleLogin}>
-            <input class="login"
-                type="text"
-                placeholder="Usuario"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-            />
-            <input class="login"
-                type="password"
-                placeholder="Conbtraseña"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <button type="submit" class="login">Iniciar sesión</button>
-            </form>
-        </div>
+    <div className="body">
+      <div className="login_circles"></div>
+      <div className="login-box">
+        <h2 className="login">Login</h2>
+        <form onSubmit={handleLogin}>
+          <input
+            className="login"
+            type="text"
+            placeholder="Usuario"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            className="login"
+            type="password"
+            placeholder="Contraseña"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit" className="login">
+            Iniciar sesión
+          </button>
+        </form>
       </div>
+    </div>
   );
 };
+
 export default Login;
