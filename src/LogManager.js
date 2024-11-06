3e3a8fc6ff42ff14ff4f3608c837cc6d7ffeef85
Debombii <158999291+Debombii@users.noreply.github.com>
2024-11-06 09:35:36 +0100

    Update LogManager.js

--- src/LogManager.js
+++ src/LogManager.js
@@ -131,6 +131,14 @@ const LogManager = () => {
           </div>
         </div>
       )}
+
+        <img
+        src="https://cdn-icons-png.flaticon.com/512/0/340.png"
+        alt="Volver"
+        className="redirect-icon"
+        onClick={handleRedirect}
+        style={{ cursor: "pointer", width: "50px", height: "50px" }} 
+      />
     </div>
   );
 };
