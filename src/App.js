a031483769c76482c44050b69c17b6d91aa291dc
Debombii <158999291+Debombii@users.noreply.github.com>
2024-11-07 08:58:01 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Editor } from "@tinymce/tinymce-react";
-import { Link, useNavigate } from "react-router-dom";
 
 const ChangelogGenerator = () => {
   const [description, setDescription] = useState("");
@@ -12,9 +11,6 @@ const ChangelogGenerator = () => {
   const [title, setTitle] = useState("");
   const [isHovered, setIsHovered] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-
-  const editorRef = useRef(null);
-  const navigate = useNavigate();
 
   const escapeHtml = (html) => {
     return html
@@ -43,13 +39,21 @@ const ChangelogGenerator = () => {
     const today = new Date();
     const month = String(today.getMonth() + 1).padStart(2, "0");
     const year = String(today.getFullYear()).slice(-2);
-    const randomNumbers = String(Math.floor(Math.random() * 1000)).padStart(4, "0");
+    const randomNumbers = String(Math.floor(Math.random() * 1000)).padStart(
+      4,
+      "0"
+    );
     return `${month}${year}-${randomNumbers}`;
   };
 
   const generateHtml = () => {
     const version = generateVersion();
 
+    const cleanContent = (content) => {
+      return content
+        .replace(/<p><p>(.*?)<\/p><\/p>/g, '<p>$1</p>') // Reemplaza <p><p>...</p></p>
+        .replace(/<p><\/p>/g, ''); // Elimina párrafos vacíos
+    };
     const html = `
       <!DOCTYPE html>
       <html lang='es'>
@@ -57,16 +61,17 @@ const ChangelogGenerator = () => {
           <meta charset='UTF-8'>
           <meta name='viewport' content='width=device-width, initial-scale=1.0'>
           <title>${version}</title>
-          <style></style>
+          <style>
+          </style>
       </head>
       <body>
           <div class='container'>
               <div class='content'>
                   <div class='version'>
-                      <h2 class="base" id="${version.trim().replace(/\s+/g, "-")}">${version}</h2>
+                      <h2 id="${version.trim().replace(/\s+/g, "-")}">${version}</h2>
                       <p class='date' id="date">${new Date().toLocaleDateString("es-ES")}</p>
                       <h3 class="titulo" id="${title}">${title}</h3>
-                      <h3 class="titular">Contenido</h3>
+                      <h3>Contenido</h3>
                       ${description}
                   </div>
               </div>
@@ -114,6 +119,11 @@ const ChangelogGenerator = () => {
     );
   };
 
+  // Función para redirigir al endpoint /logs
+  const handleRedirect = () => {
+    window.location.href = '/logs'; // Redirige a /logs
+  };
+
   return (
     <div>
       <header className="header">
@@ -124,25 +134,13 @@ const ChangelogGenerator = () => {
         <h1 className="title">Generador de Log de Cambios</h1>
       </header>
       <div className="container">
-        <div className="icon-container">
-          <img
-            src="https://cdn-icons-png.flaticon.com/512/25/25008.png"
-            alt="Eliminar Logs"
-            className="redirect-icon"
-            style={{ cursor: "pointer", width: "50px", height: "50px" }}
-            onClick={() => navigate("/logs")}
-          />
-          
-          <Link to="/modifyLogs">
-            <img
-              src="https://icons.veryicon.com/png/o/miscellaneous/currency/update-12.png"
-              alt="Modificar Logs"
-              className="redirect-icon"
-              style={{ cursor: "pointer", width: "50px", height: "50px", marginLeft: "10px" }}
-            />
-          </Link>
-        </div>
-
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/25/25008.png"
+          alt="Eliminar Logs"
+          className="redirect-icon"
+          onClick={handleRedirect}
+          style={{ cursor: "pointer", width: "50px", height: "50px" }} // Ajusta el tamaño aquí
+        />
         <h1 className="title">Generador de Log de Cambios</h1>
         <form
           onSubmit={(e) => {
@@ -172,54 +170,79 @@ const ChangelogGenerator = () => {
           <label className="label">
             Proyectos:
             <div className="checkbox-group">
-              {["MRG", "OCC", "Godiz", "GERP", "Rubicon"].map((company) => (
-                <label key={company}>
-                  <input
-                    type="checkbox"
-                    value={company}
-                    checked={selectedCompanies.includes(company)}
-                    onChange={handleCompanyChange}
-                  />
-                  <div className="custom-checkbox"></div>
-                  {company}
-                </label>
-              ))}
+              <label>
+                <input
+                  type="checkbox"
+                  value="MRG"
+                  checked={selectedCompanies.includes("MRG")}
+                  onChange={handleCompanyChange}
+                />
+                <div className="custom-checkbox"></div>
+                MRG
+              </label>
+              <label>
+                <input
+                  type="checkbox"
+                  value="OCC"
+                  checked={selectedCompanies.includes("OCC")}
+                  onChange={handleCompanyChange}
+                />
+                <div className="custom-checkbox"></div>
+                OCC
+              </label>
+              <label>
+                <input
+                  type="checkbox"
+                  value="Godiz"
+                  checked={selectedCompanies.includes("Godiz")}
+                  onChange={handleCompanyChange}
+                />
+                <div className="custom-checkbox"></div>
+                Godiz
+              </label>
+              <label>
+                <input
+                  type="checkbox"
+                  value="GERP"
+                  checked={selectedCompanies.includes("GERP")}
+                  onChange={handleCompanyChange}
+                />
+                <div className="custom-checkbox"></div>
+                GERP
+              </label>
+              <label>
+                <input
+                  type="checkbox"
+                  value="Rubicon"
+                  checked={selectedCompanies.includes("Rubicon")}
+                  onChange={handleCompanyChange}
+                />
+                <div className="custom-checkbox"></div>
+                Rubicon
+              </label>
             </div>
           </label>
           <label className="label">
-            Contenido:
-              <Editor
-  apiKey="7a1g5nuzi6ya3heq0tir17f9lxstt7xlljnlavx1agc1n70n"
-  value={description}
-  onInit={(evt, editor) => (editorRef.current = editor)}
-  init={{
-    height: 300,
-    menubar: false,
-    plugins: [
-      "advlist autolink lists link image charmap print preview anchor",
-      "searchreplace visualblocks code fullscreen",
-      "insertdatetime media table paste code help wordcount",
-      "textcolor",
-      "autoresize",
-      "autosave",
-      "fontsize",
-    ],
-    toolbar:
-      "undo redo | formatselect | bold italic backcolor | fontsize | \
-      alignleft aligncenter alignright alignjustify | \
-      bullist numlist outdent indent | removeformat | help | \
-      forecolor backcolor | link image",
-    fontsize_formats: "8pt 10pt 12pt 14pt 18pt 24pt 36pt",
-    autoresize: {
-      enabled: true,
-      min_height: 200, 
-      max_height: 600, 
-    },
-    autoresize_bottom_margin: 10, 
-  }}
-  onEditorChange={(newValue) => setDescription(newValue)}
-  required
-/>
+            Descripción:
+            <Editor
+              apiKey="7a1g5nuzi6ya3heq0tir17f9lxstt7xlljnlavx1agc1n70n"
+              value={description}
+              init={{
+                height: 300,
+                menubar: false,
+                plugins: [
+                  "advlist autolink lists link image charmap print preview anchor",
+                  "searchreplace visualblocks code fullscreen",
+                  "insertdatetime media table paste code help wordcount",
+                ],
+                toolbar:
+                  "undo redo | formatselect | bold italic backcolor | \
+                  alignleft aligncenter alignright alignjustify | \
+                  bullist numlist outdent indent | removeformat | help",
+              }}
+              onEditorChange={(newValue) => setDescription(newValue)}
+              required
+            />
           </label>
           <div className="button-container">
             <button
@@ -250,15 +273,15 @@ const ChangelogGenerator = () => {
             />
             <div className="button-container">
               <button
-  onClick={sendJson}
-  className={`download-button ${
-    isHovered === "download" ? "download-button-hover" : ""
-  }`}
-  onMouseEnter={() => setIsHovered("download")}
-  onMouseLeave={() => setIsHovered("")}
->
-  Enviar JSON
-</button>
+                onClick={sendJson}
+                className={`download-button ${
+                  isHovered === "download" ? "download-button-hover" : ""
+                }`}
+                onMouseEnter={() => setIsHovered("download")}
+                onMouseLeave={() => setIsHovered("")}
+              >
+                Enviar JSON
+              </button>
             </div>
           </div>
         )}
